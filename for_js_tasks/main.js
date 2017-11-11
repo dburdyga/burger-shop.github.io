0dd8e8065cbbcbe61fd496cdf17f6c06017980e4
dburdyga <dariaburdyga@gmail.com>
2017-11-12 00:45:57 +0300

    12112017

--- for_js_tasks/main.js
+++ for_js_tasks/main.js
@@ -1,1 +1,10 @@
  console.log ('привет');
+
+ var testString = "Тестовая строка";
+ var lettersArr = testString.split('')
+
+ 
+
+ for (var i=0; i < lettersArr.length; i++) {
+ console.log(lettersArr [i]);
+ }
