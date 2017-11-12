27bb1a30633f7eb9e2e091ec52b03ccb6e89aea8
dburdyga <dariaburdyga@gmail.com>
2017-11-12 23:20:13 +0300

    js 1211

--- for_js_tasks/main.js
+++ for_js_tasks/main.js
@@ -1,10 +1,93 @@
- console.log ('привет');
+ // i
 
- var testString = "Тестовая строка";
+ /*
+ 
+ var testString = "Яблоко"
  var lettersArr = testString.split('')
 
+ var vowels = ["а", "е", "и", "о", "у", "я", "ё", "ю", "й", "ы" ]
+
+ var result = "" // e
  
 
  for (var i=0; i < lettersArr.length; i++) {
- console.log(lettersArr [i]);
+ 	var currentLetter = lettersArr[i]
+
+ 	if (vowels.indexOf(currentLetter.toLowerCase()) !== -1) {
+ 	result = result + currentLetter
+ 	}
  }
+
+  console.log(result);
+
+  */
+
+
+  /*
+  function test (a) {
+  	var b = a + 10;
+
+  	//code
+  	//code
+  	//code
+  	//code
+  	//code
+
+  	return b;
+  }
+
+  var resultFunc = test(10);
+
+  console.log(resultFunc);
+   */
+
+
+   /*
+   var users = [
+   {
+
+   	name: "Алексей",
+   	salary: 500
+
+   },
+   {
+
+   	name: "Виктор",
+   	salary: 1300
+
+   },
+   {
+   	name: "Иван",
+   	salary: 1500
+   }
+   ];
+
+   function filterBySalary(array, salary) {
+   	var resultArr = [];
+
+   	for (var i = 0; i < users.length; i++) {
+   	 var user = array[i]
+
+   	if(user.salary >= salary) {
+   		resultArr.push(user.name)
+   	}
+
+   	}
+   
+   return resultArr
+
+   }
+
+   var fireThem = filterBySalary(users, 1400)
+
+   console.log(fireThem)
+     */
+
+     function isHtml(path, ext) {
+     	var isHtmlOrNot = path.slice(ext.length) === ext
+     	return isHtmlOrNot
+     }
+
+     var result = isHtml('/users/download/for_js.html', 'jpeg')
+
+     console.log(result)
