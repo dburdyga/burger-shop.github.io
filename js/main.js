db7fe5ad5cbecfd4afaa7f1b384f53c5d1e13768
dburdyga <dariaburdyga@gmail.com>
2017-11-21 17:43:54 +0300

    menu 17/43

--- js/main.js
+++ js/main.js
@@ -77,21 +77,47 @@ function f_ac(){
 
 
 
- 
 
 $(document).ready(function(){
-    $('.menu-acco__trigger').on('click', f_ccc);
+    $('.menu-acco__trigger').on('click',f_ccc);
+    
 });
-
+ 
 function f_ccc(){
- $(this).next().toggleClass('menu-acco__content--visible');
- $(this).toggleClass('menu-acco__text--visible');
-  
+    $('.menu-acco__trigger').next().removeClass('menu-acco__content--visible');
+    $('.menu-acco__trigger').removeClass('.menu-acco__trigger--visible');
+    $(this).next().toggleClass('menu-acco__content--visible');
+    $(this).toggleClass('.menu-acco__trigger--visible');    
 }
 
+ 
+   
+
+
+ 
+
+// $(document).ready(function(){
+//     $('.menu-acco__trigger').on('click', f_ccc);
+// });
+
+// function f_ccc(){
+//  $(this).next().toggleClass('menu-acco__content--visible');
+//  $(this).toggleClass('menu-acco__text--visible');
+  
+// }
 
 
 
+
+// $(document).ready(function(){
+//     $('.menu-acco__trigger').on('click', f_ccc);
+// });
+
+// function f_ccc(){
+//  $(this).next().toggleClass('menu-acco__content--visible');
+//  $(this).toggleClass('menu-acco__text--visible');
+  
+// }
 
 ymaps.ready(init);
 
