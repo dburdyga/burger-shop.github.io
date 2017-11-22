3536255b3dddc1a00ff86739462e40ce34dd0d03
dburdyga <dariaburdyga@gmail.com>
2017-11-22 15:31:24 +0300

    15/31

--- js/main.js
+++ js/main.js
@@ -78,7 +78,9 @@ function f_ac(){
 
 
 $(document).ready(function(){
-    $('.menu-acco__trigger').on('click',f_ccc);
+    $('.menu-acco__trigger').mouseover(f_ccc);
+    $('.menu-acco__content').mouseout(f_cc);
+    $('.menu-acco__trigger').mouseout(f_cc);
     
 });
  
@@ -87,6 +89,13 @@ function f_ccc(){
     $('.menu-acco__trigger').removeClass('.menu-acco__content--visible');
     $(this).next().toggleClass('menu-acco__content--visible');
     $(this).toggleClass('.menu-acco__content--visible');    
+}
+
+function f_сc(){ 
+    // $('.menu-acco__content').next().removeClass('menu-acco__content--visible');
+    // $('.menu-acco__triger').removeClass('menu-acco__content--visible');
+    $('.menu-acco__content--visible').next().hide();
+    $('.menu-acco__trigger').next().hide();
 }
 
  
